905608b83e789ddab7d6296b1945238e1fe43b21
Habibaferdausi <ritu.pramanik1@gmail.com>
2023-05-10 23:49:44 +0600

    add firebase

--- src/Components/firebase/firebase.config.js
+++ src/Components/firebase/firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your 000000000web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDY0s2cIw-r5MdbrFKLefunXqEo-LLRXCQ",
-  authDomain: "car-care-d6cc3.firebaseapp.com",
-  projectId: "car-care-d6cc3",
-  storageBucket: "car-care-d6cc3.appspot.com",
-  messagingSenderId: "1060229887456",
-  appId: "1:1060229887456:web:caaafb1e996f15b4ae2785",
+  apiKey: import.meta.env.VITE_apiKey,
+  authDomain: import.meta.env.VITE_authDomain,
+  projectId: import.meta.env.VITE_projectId,
+  storageBucket: import.meta.env.VITE_storageBucket,
+  messagingSenderId: import.meta.env.VITE_messagingSenderId,
+  appId: import.meta.env.VITE_appId,
 };
 
 // Initialize Firebase
