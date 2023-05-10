9b652559c8117225f6b3ef6bfb7913ae03c11a73
Habibaferdausi <ritu.pramanik1@gmail.com>
2023-05-10 21:45:23 +0600

    add login and register form

--- src/Components/Routers/router.jsx
+++ src/Components/Routers/router.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../Layout/Main";
 import Home from "../Home/Home";
+import SignIn from "../Login/SignIn";
+import SignUp from "../Login/SignUp";
 
 const router = createBrowserRouter([
   {
@@ -11,6 +13,14 @@ const router = createBrowserRouter([
       {
         path: "/",
         element: <Home></Home>,
+      },
+      {
+        path: "login",
+        element: <SignIn></SignIn>,
+      },
+      {
+        path: "signup",
+        element: <SignUp></SignUp>,
       },
     ],
   },
