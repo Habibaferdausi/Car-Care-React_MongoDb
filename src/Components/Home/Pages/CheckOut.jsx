afb0caab129b1971e520cf043a03146e958d98de
Habibaferdausi <ritu.pramanik1@gmail.com>
2023-05-12 14:02:38 +0600

    update design

--- src/Components/Home/Pages/CheckOut.jsx
+++ src/Components/Home/Pages/CheckOut.jsx
@@ -44,9 +44,32 @@ const CheckOut = () => {
 
   return (
     <div>
-      <h2 className="text-center text-3xl text-red-500">Check Out: {title} </h2>
-      <form onSubmit={handleBookService}>
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+      <div
+        className="bg-cover bg-center relative rounded flex items-center "
+        style={{
+          backgroundImage: `url(https://img.freepik.com/free-photo/muscular-car-service-worker-repairing-vehicle_146671-19705.jpg?w=826&t=st=1683877961~exp=1683878561~hmac=d20b060babf427fee9b987527c85d4894eedf241b66b872796270e13f8f8dc5a)`,
+          height: "200px",
+        }}
+      >
+        {/* Dark gradient overlay */}
+        <div className="absolute inset-0 bg-gradient-to-t from-black to-transparent"></div>
+
+        <div className="text-start rounded flex items-center relative">
+          <h1 className="text-white text-2xl lg:text-5xl lg:ms-5 lg:ps  p-4  mb-5 mt-12 font-semibold ">
+            Check Out
+          </h1>
+        </div>
+      </div>
+      {/* banner end */}
+
+      <h2 className="text-center mt-10 mb-10 font-bold text-3xl text-red-500">
+        Service Name: <span className="ms-3 text-black">{title}</span>
+      </h2>
+      <form
+        onSubmit={handleBookService}
+        className="mt-10 bg-slate-200 p-11 rounded"
+      >
+        <div className="grid grid-cols-1 md:grid-cols-2  gap-6">
           <div className="form-control">
             <label className="label">
               <span className="label-text">Name</span>
@@ -89,7 +112,7 @@ const CheckOut = () => {
         </div>
         <div className="form-control mt-6">
           <input
-            className="btn btn-primary btn-block"
+            className="btn bg-red-500 w-2/3 mx-auto border border-0 mt-5 btn-block"
             type="submit"
             value="Order Confirm"
           />
